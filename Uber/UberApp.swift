408e5cde9fb6c94e2acd27a0525e7d537fddc8af
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-03-15 20:47:20 +0530

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Mohammed Mubashir on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
