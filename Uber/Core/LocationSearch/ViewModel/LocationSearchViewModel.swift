dcddd9238d91fb133ce7fef98f55081db09df8cb
Mohammed Mubashir <mohammedmubashir204@gmail.com>
2023-03-18 11:23:49 +0530

    - Fix a bug ( list shows an empty address even before typing the address in the search bar )

--- Uber/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
+++ Uber/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -8,36 +8,58 @@
 import Foundation
 import MapKit
 
-class LocationSearchViewModel : NSObject, ObservableObject {
-    
+class LocationSearchViewModel: NSObject, ObservableObject {
+
     //MARK: properties
-    
-    @Published var results = [MKLocalSearchCompletion()]
-    @Published var selectedLocation : String?
-    
+
+    @Published var results = [MKLocalSearchCompletion]()
+    @Published var selectedLocation: String?
+
     private let searchCompleter = MKLocalSearchCompleter()
-    var queryFragment : String = ""  {
+    var queryFragment: String = "" {
         didSet {
             searchCompleter.queryFragment = queryFragment
         }
     }
-    
+
     override init() {
         super.init()
         searchCompleter.delegate = self
         searchCompleter.queryFragment = queryFragment
     }
-    
+
     // MARK: Helpers
-     
-    func selectLocation(_ location:String){
-        self.selectedLocation = location
-        print("DEBUG: selected location is \(selectedLocation)")
+
+    func selectLocation(_ localSearch: MKLocalSearchCompletion) {
+        selectedLocation = localSearch.title + " " + localSearch.subtitle
+
+        locationSearch { response, error in
+            if let error = error {
+                print("DEBUG: location search failed \(error.localizedDescription)")
+                return
+            }
+
+            guard let item = response?.mapItems.first else { return }
+            let coordinate = item.placemark.coordinate
+
+            print("DEBUG: location coordinates \(coordinate.longitude), \(coordinate.latitude)")
+        }
     }
-    
-    
-    
+
+    func locationSearch(completion: @escaping MKLocalSearch.CompletionHandler) {
+        guard let selectedLocation = selectedLocation else {
+            self.results = []
+            return
+        }
+
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = selectedLocation
+        let search = MKLocalSearch(request: searchRequest)
+
+        search.start(completionHandler: completion)
+    }
 }
+
 
 // MARK: MKLocalSearchCompleterDelegate
 
